cbd792fd306d0dd0d6e27c70bb32362df4d4e7fa
Evandro Myller <emyller@d7.dev>
2022-03-02 22:35:13 -0300

    Output the name of the cluster

--- _outputs.tf
+++ _outputs.tf
@@ -2,6 +2,14 @@ output "arn" {
   value = aws_ecs_cluster.main.arn
 }
 
+output "name" {
+  description = "Name of the cluster."
+  value = split("--", join("--", [  # Force-wait for resource
+    aws_ecs_cluster.main.arn,  # The ARN only exists after resource
+    aws_ecs_cluster.main.name,
+  ]))[1]
+}
+
 output "instances_security_group_id" {
   value = module.security_group_ecs_instances.id
 }
